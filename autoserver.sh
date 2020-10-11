fe62110215d015cb23c8c4e3fce402222dbcd914
quisys <56238271+quisys@users.noreply.github.com>
2020-10-11 12:47:08 -0400

    Update autoserver.sh

--- autoserver.sh
+++ autoserver.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 mkdir minecraft-server
 wget https://papermc.io/api/v1/paper/1.16.3/223/download
+mv download minecraft-server/server.jar
+cd minecraft-server
+java -jar server.jar
+rm eula.txt
+cat > minecraft-server/eula.txt << EOF
+eula=true
+EOF
