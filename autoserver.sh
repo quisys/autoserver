df8520ddba8c69ca9e41431995c421cd4bbf1812
quisys <56238271+quisys@users.noreply.github.com>
2021-03-10 08:52:34 -0500

    Update autoserver.sh

--- autoserver.sh
+++ autoserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir ../minecraft-server
-cd ../minecraft-server
+mkdir ~/minecraft-server
+cd ~/minecraft-server
 wget https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
 echo "eula=true" > eula.txt
 echo "done! run java -jar server.jar in the new minecraft-server directory to start it :)"
