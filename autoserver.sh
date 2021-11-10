4b09525383a20351b8f9baa28f26a7065e317f6d
finn <56238271+quisys@users.noreply.github.com>
2021-11-09 21:24:33 -0500

    Update autoserver.sh

--- autoserver.sh
+++ autoserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir ~/minecraft-server
 cd ~/minecraft-server
-wget https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
+wget https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/377/downloads/paper-1.17.1-377.jar
 echo "eula=true" > eula.txt
 echo "done! run java -jar server.jar in the new ~/minecraft-server directory to start it :)"
