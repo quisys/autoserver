fef3c1ed8ed0214d4dfe1befda256cb96de6c6c0
finn <56238271+quisys@users.noreply.github.com>
2021-11-09 21:26:29 -0500

    Update autoserver.sh

--- autoserver.sh
+++ autoserver.sh
@@ -3,4 +3,4 @@ mkdir ~/minecraft-server
 cd ~/minecraft-server
 wget https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/377/downloads/paper-1.17.1-377.jar
 echo "eula=true" > eula.txt
-echo "done! run java -jar server.jar in the new ~/minecraft-server directory to start it :)"
+echo "done! run java -jar paper-1.17.1-377.jar in the new ~/minecraft-server directory to start it :)"
